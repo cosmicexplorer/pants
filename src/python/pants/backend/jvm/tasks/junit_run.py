a364055ff34777923511a70495ac0c7ce0817adc
Eric Ayers <zundel@squareup.com>
2016-05-18 20:47:30 -0400

    Bump junit-runner to 1.0.7

--- src/python/pants/backend/jvm/tasks/junit_run.py
+++ src/python/pants/backend/jvm/tasks/junit_run.py
@@ -108,7 +108,7 @@ class JUnitRun(TestRunnerTaskMixin, JvmToolTaskMixin, JvmTask):
     cls.register_jvm_tool(register,
                           'junit',
                           classpath=[
-                            JarDependency(org='org.pantsbuild', name='junit-runner', rev='1.0.5'),
+                            JarDependency(org='org.pantsbuild', name='junit-runner', rev='1.0.7'),
                           ],
                           main=JUnitRun._MAIN,
                           # TODO(John Sirois): Investigate how much less we can get away with.
