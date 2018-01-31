629ff4675fea232b4d4ceff169ed21c0a03056b4
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-01-31 01:02:38 -0800

    clean up PythonDistribution and add c_sources field

--- src/python/pants/backend/python/targets/python_distribution.py
+++ src/python/pants/backend/python/targets/python_distribution.py
@@ -5,16 +5,11 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from pex.interpreter import PythonIdentity
-from twitter.common.collections import maybe_list
-
-from pants.base.exceptions import TargetDefinitionException
+from pants.backend.python.targets.python_target import PythonTarget
 from pants.base.payload import Payload
-from pants.base.payload_field import PrimitiveField
-from pants.build_graph.target import Target
 
 
-class PythonDistribution(Target):
+class PythonDistribution(PythonTarget):
   """A Python distribution target that accepts a user-defined setup.py."""
 
   default_sources_globs = '*.py'
@@ -27,7 +22,7 @@ class PythonDistribution(Target):
                address=None,
                payload=None,
                sources=None,
-               compatibility=None,
+               c_sources=None,
                **kwargs):
     """
     :param address: The Address that maps to this Target in the BuildGraph.
@@ -38,26 +33,15 @@ class PythonDistribution(Target):
     :param sources: Files to "include". Paths are relative to the
       BUILD file's directory.
     :type sources: ``Fileset`` or list of strings. Must include setup.py.
-    :param compatibility: either a string or list of strings that represents
-      interpreter compatibility for this target, using the Requirement-style
-      format, e.g. ``'CPython>=3', or just ['>=2.7','<3']`` for requirements
-      agnostic to interpreter class.
     """
     payload = payload or Payload()
     payload.add_fields({
-      'sources': self.create_sources_field(sources, address.spec_path, key_arg='sources'),
-      'compatibility': PrimitiveField(maybe_list(compatibility or ()))
+      'c_sources': self.create_sources_field(sources, address.spec_path, key_arg='c_sources'),
     })
-    super(PythonDistribution, self).__init__(address=address, payload=payload, **kwargs)
+    super(PythonDistribution, self).__init__(
+      address=address, payload=payload, sources=sources, **kwargs)
 
     if not 'setup.py' in sources:
       raise TargetDefinitionException(
         self, 'A setup.py in the top-level directory relative to the target definition is required.'
       )
-
-    # Check that the compatibility requirements are well-formed.
-    for req in self.payload.compatibility:
-      try:
-        PythonIdentity.parse_requirement(req)
-      except ValueError as e:
-        raise TargetDefinitionException(self, str(e))
