ed1d09770f5fa53f24f9e52fc26c05e100e7e50b
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-03-09 00:44:25 -0800

    take a few more stabs

--- src/python/pants/backend/native/subsystems/xcode_linker.py
+++ src/python/pants/backend/native/subsystems/xcode_linker.py
@@ -8,22 +8,12 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 import os
 
 from pants.backend.native.clang import Clang
-from pants.binaries.host_installed_tool_base import HostInstalledToolBase
+from pants.binaries.host_installed_tool_base import HostInstalledBinaryTool
 from pants.util.contextutil import environment_as, temporary_dir
 from pants.util.memo import memoized_property
-from pants.util.process_handler import subprocess
 
 
-HELLO_WORLD_C_SOURCE = '''
-#include <stdio.h>
-
-int main() {
-    printf("%s\n", "Hello, world!");
-}
-'''
-
-
-class XCodeLinker(HostInstalledToolchainBase):
+class XCodeLinker(HostInstalledBinaryTool):
   options_scope = 'xcode-linker'
   name = 'ld'
   default_tool_path = '/usr/bin/ld'
@@ -35,30 +25,21 @@ Click "Install" in the dialog box that pops up, and wait for installation to
 complete.
 '''
 
-  @classmethod
-  def subsystem_dependencies(cls):
-    return super(XCodeLinker, cls).subsystem_dependencies() + (Clang.scoped(cls))
+  # def get_validate_host_tool(self):
+  #   ld_host_binary = super(XCodeLinker, self).get_validate_host_tool()
 
-  @memoized_property
-  def _clang(self):
-    return Clang.scoped_instance(self)
+  #   with temporary_dir() as tmpdir:
+  #     c_source = os.path.join(tmpdir, 'hello.c')
+  #     with open(c_source, 'w') as fp:
+  #       fp.write(HELLO_WORLD_C_SOURCE)
 
-  def validate_host_tool(self, host_path):
-    ld_host_binary = super(XCodeLinker, self).validate_host_tool(
-      host_path, context)
+  #     ld_bin_dir = os.path.dirname(ld_host_binary)
+  #     scrubbed_path = '{}:{}'.format(self._clang.bin_dir(), ld_bin_dir)
+  #     with environment_as(PATH=scrubbed_path):
+  #       subprocess.call(['clang', 'hello.c', '-o', 'hello'],
+  #                       cwd=tmpdir)
+  #       hello_stdout = subprocess.check_output(['./hello'], cwd=tmpdir)
+  #       if hello_stdout != 'Hello, World!\n':
+  #         raise Exception("BAD STDOUT: '{}'".format(hello_stdout))
 
-    with temporary_dir() as tmpdir:
-      c_source = os.path.join(tmpdir, 'hello.c')
-      with open(c_source, 'w') as fp:
-        fp.write(HELLO_WORLD_C_SOURCE)
-
-      ld_bin_dir = os.path.dirname(ld_host_binary)
-      scrubbed_path = '{}:{}'.format(self._clang.bin_dir(), ld_bin_dir)
-      with environment_as(PATH=scrubbed_path):
-        subprocess.call(['clang', 'hello.c', '-o', 'hello'],
-                        cwd=tmpdir)
-        hello_stdout = subprocess.check_output(['./hello'], cwd=tmpdir)
-        if hello_stdout != 'Hello, World!\n':
-          raise Exception("BAD STDOUT: '{}'".format(hello_stdout))
-
-    return ld_host_binary
+  #   return ld_host_binary
--- src/python/pants/binaries/host_installed_tool_base.py
+++ src/python/pants/binaries/host_installed_tool_base.py
@@ -28,42 +28,11 @@ class HostInstalledToolBase(BinaryToolBase):
 
   :API: public
   """
-
-  # NB: gotta set that options_scope
-  name = None
-
-  default_tool_path = None
-
   # If the toolchain is unavailable on the current host, display these simple,
   # complete instructions to obtain the toolchain. Must be a string.
   complete_install_instructions = None
 
-  @classmethod
-  def register_options(cls, register):
-    super(HostInstalledToolBase, cls).register_options(register)
-
-    register('--host-filesystem-path', advanced=True,
-             type=str, default=cls.default_tool_path,
-             # TODO: fix this message when remoting happens
-             help='Path to this tool on the host filesystem. Path must point '
-                  'to an executable file on the filesystem running Pants.')
-
-  def digest(self):
-    return hashlib.sha1()
-
   class BootstrapError(Exception): pass
-
-  # Subclasses should override this and call super() beforehand. This validation
-  # is memoized, so the validation may contain e.g. filesystem calls.
-  def validate_host_tool(self, host_path):
-    if os.path.isfile(host_path) and os.access(host_path, os.X_OK):
-      return host_path
-    raise self.BootstrapError(
-      "Path '{}' must be an executable file on the host filesystem."
-      .format(host_path))
-
-  def host_tool_path(self):
-    return self.get_options().host_filesystem_path
 
   INSTALL_ERR_WITH_INSTRUCTIONS_BOILERPLATE = """Bootstrap of tool '{name}' failed: {error_desc}
 
@@ -86,25 +55,58 @@ file an issue!
       error_desc=error_desc,
       install_instrs=self._make_install_instructions())
 
+  # Subclasses should override this and call super() beforehand.
+  @abstractmethod
+  def try_get_host_tool(self): pass
+
   class UserFriendlyBootstrapError(Exception): pass
 
   @memoized_method
   def _get_host_tool(self):
     try:
-      return self.validate_host_tool(self.host_tool_path())
+      return self.try_get_host_tool()
     except self.BootstrapError as e:
       msg = self._installation_required_err_msg(self._get_name(), e.message)
       raise self.UserFriendlyBootstrapError(msg, e)
 
   @memoized_method
-  def version(self, context=None):
-    return hash_file(self._get_host_tool(), digest=self.digest())
-
-  @memoized_method
   def select_for_version(self, version):
-    validated_host_path = self.validate_host_tool()
+    validated_host_tool = self._get_host_tool()
+
+    # TODO: make BinaryUtil handle different types of host-installed
+    # tools (e.g. directories) and not just a single binary path.
     return self._binary_util.select_host_installed(
       supportdir=self.get_support_dir(),
       name=self._get_name(),
       version=version,
-      host_binary_path=validated_host_path)
+      host_binary_path=validated_host_tool)
+
+
+class HostInstalledBinaryTool(HostInstalledToolBase):
+
+  default_tool_path = None
+
+  @classmethod
+  def register_options(cls, register):
+    super(HostInstalledBinaryTool, cls).register_options(register)
+
+    register('--host-filesystem-path', advanced=True,
+             type=str, default=cls.default_tool_path,
+             # TODO: fix this message when remoting happens
+             help='Path to this tool on the host filesystem. Path must point '
+                  'to an executable file on the filesystem running Pants.')
+
+  def try_get_host_tool(self):
+    host_path = self.get_options().host_filesystem_path
+    if os.path.isfile(host_path) and os.access(host_path, os.X_OK):
+      return host_path
+    raise HostInstalledToolBase.BootstrapError(
+      "Path '{}' must be an executable file on the host filesystem."
+      .format(host_path))
+
+  def digest(self):
+    return hashlib.sha1()
+
+  @memoized_method
+  def version(self):
+    return hash_file(self._get_host_tool(), digest=self.digest())
