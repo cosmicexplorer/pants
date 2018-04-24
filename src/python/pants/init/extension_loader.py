81c71ea519c5615e065d973025ff259f35a66171
Daniel McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-04-24 13:34:03 -0700

    extend EngineInitializer to add more rules from a backend

--- src/python/pants/init/extension_loader.py
+++ src/python/pants/init/extension_loader.py
@@ -6,6 +6,7 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
                         unicode_literals, with_statement)
 
 import importlib
+import logging
 import traceback
 
 from pkg_resources import Requirement
@@ -13,6 +14,9 @@ from twitter.common.collections import OrderedSet
 
 from pants.base.exceptions import BackendConfigurationError
 from pants.build_graph.build_configuration import BuildConfiguration
+
+
+logger = logging.getLogger(__name__)
 
 
 class PluginLoadingError(Exception): pass
