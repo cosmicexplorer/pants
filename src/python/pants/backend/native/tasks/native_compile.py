d525c6f01f33f943b2347e4c3639c1f729a8c5f3
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-07-20 08:55:54 -0700

    add testing for the platform fix

--- src/python/pants/backend/native/tasks/native_compile.py
+++ src/python/pants/backend/native/tasks/native_compile.py
@@ -13,6 +13,7 @@ from pants.backend.native.config.environment import Executable
 from pants.backend.native.targets.native_library import NativeLibrary
 from pants.backend.native.tasks.native_external_library_fetch import NativeExternalLibraryFetch
 from pants.backend.native.tasks.native_task import NativeTask
+from pants.base.build_environment import get_buildroot
 from pants.base.exceptions import TaskError
 from pants.base.workunit import WorkUnit, WorkUnitLabel
 from pants.build_graph.dependency_context import DependencyContext
@@ -59,6 +60,10 @@ class NativeCompile(NativeTask, AbstractClass):
   @classmethod
   def product_types(cls):
     return [ObjectFiles, NativeTargetDependencies]
+
+  @classmethod
+  def prepare(cls, options, round_manager):
+    round_manager.optional_data(NativeExternalLibraryFetch.NativeExternalLibraryFiles)
 
   @property
   def cache_target_dirs(self):
@@ -189,6 +194,9 @@ class NativeCompile(NativeTask, AbstractClass):
     return self.get_compiler()
 
   def _get_third_party_include_dirs(self, external_libs_product):
+    if not external_libs_product:
+      return []
+
     directory = external_libs_product.include_dir
     return [directory] if directory else []
 
@@ -215,13 +223,17 @@ class NativeCompile(NativeTask, AbstractClass):
 
     # We are going to execute in the target output, so get absolute paths for everything.
     # TODO: If we need to produce static libs, don't add -fPIC! (could use Variants -- see #5788).
+    buildroot = get_buildroot()
     argv = (
       [compiler.exe_filename] +
       compiler.extra_args +
       err_flags +
       ['-c', '-fPIC'] +
-      ['-I{}'.format(os.path.abspath(inc_dir)) for inc_dir in compile_request.include_dirs] +
-      [os.path.abspath(src) for src in compile_request.sources])
+      [
+        '-I{}'.format(os.path.join(buildroot, inc_dir))
+        for inc_dir in compile_request.include_dirs
+      ] +
+      [os.path.join(buildroot, src) for src in compile_request.sources])
 
     self.context.log.debug("compile argv: {}".format(argv))
 
