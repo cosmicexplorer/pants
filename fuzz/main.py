f9ba93ad9e943e1513c40845ddc893198452c283
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-09-16 17:56:09 -0700

    the runner thinks we're in the pants repo insead of the one we just checked out!

--- fuzz/main.py
+++ fuzz/main.py
@@ -1,15 +1,13 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import os
 import sys
 import time
 
 import afl
-from fuzz.fuzz import untar_stream_into_tmp_dir
+from fuzz import untar_stream_into_tmp_dir
 
 from pants.base.exiter import Exiter
 from pants.bin.pants_runner import PantsRunner
-from pants.util.contextutil import maybe_profiled
 
 
 def main():
@@ -21,16 +19,19 @@ def main():
     exiter = Exiter()
     exiter.set_except_hook()
 
-    pants_run_env = os.environ.copy()
+    # pants_run_env = os.environ.copy()
+    pants_run_env = {}
     pants_run_env['PANTS_ENABLE_PANTSD'] = 'False'
 
-    pants_smoke_test_cmd = ['./pants', 'goals']
+    # pants_smoke_test_cmd = ['./pants', 'run', '//:bin']
+    # TODO: currently this just lists everything in the current pants repo, not the one we just
+    # untarred into a temp dir!
+    pants_smoke_test_cmd = ['./pants', 'list', '::']
 
-    with maybe_profiled(os.environ.get('PANTSC_PROFILE')):
-      try:
-        PantsRunner(exiter,
-                    args=pants_smoke_test_cmd,
-                    env=pants_run_env,
-                    start_time=start_time).run()
-      except KeyboardInterrupt:
-        exiter.exit_and_fail('Interrupted by user.')
+    try:
+      PantsRunner(exiter,
+                  args=pants_smoke_test_cmd,
+                  env=pants_run_env,
+                  start_time=start_time).run()
+    except KeyboardInterrupt:
+      exiter.exit_and_fail('Interrupted by user.')
