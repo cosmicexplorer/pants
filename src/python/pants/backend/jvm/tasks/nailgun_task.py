30cd3176a28ce89537c022820ec8f3660267c08c
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-12-04 04:24:40 -0800

    add graal jvm executor

--- src/python/pants/backend/jvm/tasks/nailgun_task.py
+++ src/python/pants/backend/jvm/tasks/nailgun_task.py
@@ -6,10 +6,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 
+from pants.backend.jvm.subsystems.graal import GraalCE
 from pants.backend.jvm.tasks.jvm_tool_task_mixin import JvmToolTaskMixin
 from pants.base.exceptions import TaskError
+from pants.base.workunit import WorkUnitLabel
+from pants.init.subprocess import Subprocess
 from pants.java import util
-from pants.java.executor import SubprocessExecutor
+from pants.java.executor import GraalExecutor, SubprocessExecutor
 from pants.java.jar.jar_dependency import JarDependency
 from pants.java.nailgun_executor import NailgunExecutor, NailgunProcessGroup
 from pants.process.subprocess import Subprocess
@@ -23,8 +26,9 @@ class NailgunTaskBase(JvmToolTaskMixin, TaskBase):
   NAILGUN = 'nailgun'
   SUBPROCESS = 'subprocess'
   HERMETIC = 'hermetic'
+  GRAAL = 'graal'
 
-  class ExecutionStrategy(enum([NAILGUN, SUBPROCESS, HERMETIC])): pass
+  class ExecutionStrategy(enum([NAILGUN, SUBPROCESS, HERMETIC, GRAAL])): pass
 
   @classmethod
   def register_options(cls, register):
@@ -52,7 +56,14 @@ class NailgunTaskBase(JvmToolTaskMixin, TaskBase):
 
   @classmethod
   def subsystem_dependencies(cls):
-    return super(NailgunTaskBase, cls).subsystem_dependencies() + (Subprocess.Factory,)
+    return super(NailgunTaskBase, cls).subsystem_dependencies() + (
+      Subprocess.Factory,
+      GraalCE.scoped(cls),
+    )
+
+  @memoized_property
+  def _graal_ce(self):
+    return GraalCE.scoped_instance(self)
 
   def __init__(self, *args, **kwargs):
     """
@@ -71,7 +82,7 @@ class NailgunTaskBase(JvmToolTaskMixin, TaskBase):
   def execution_strategy(self):
     return self.execution_strategy_enum.value
 
-  def create_java_executor(self, dist=None):
+  def create_java_executor(self, dist=None, input_fingerprint=None):
     """Create java executor that uses this task's ng daemon, if allowed.
 
     Call only in execute() or later. TODO: Enforce this.
@@ -85,11 +96,18 @@ class NailgunTaskBase(JvmToolTaskMixin, TaskBase):
                              dist,
                              connect_timeout=self.get_options().nailgun_timeout_seconds,
                              connect_attempts=self.get_options().nailgun_connect_attempts)
+    elif self.execution_strategy == self.GRAAL:
+      return GraalExecutor(dist, self._graal_ce, input_fingerprint=input_fingerprint)
     else:
       return SubprocessExecutor(dist)
 
+  _extra_workunit_labels = {
+    GraalExecutor: [WorkUnitLabel.RUN],
+  }
+
   def runjava(self, classpath, main, jvm_options=None, args=None, workunit_name=None,
-              workunit_labels=None, workunit_log_config=None, dist=None):
+              workunit_labels=None, workunit_log_config=None, dist=None,
+              input_fingerprint=None):
     """Runs the java main using the given classpath and args.
 
     If --execution-strategy=subprocess is specified then the java main is run in a freshly spawned
@@ -98,7 +116,12 @@ class NailgunTaskBase(JvmToolTaskMixin, TaskBase):
 
     :API: public
     """
-    executor = self.create_java_executor(dist=dist)
+    executor = self.create_java_executor(dist=dist, input_fingerprint=input_fingerprint)
+
+    for executor_cls, labels in self._extra_workunit_labels.items():
+      if isinstance(executor, executor_cls):
+        workunit_labels = workunit_labels or []
+        workunit_labels.extend(labels)
 
     # Creating synthetic jar to work around system arg length limit is not necessary
     # when `NailgunExecutor` is used because args are passed through socket, therefore turning off
