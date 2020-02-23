4994b3b4be8a12b3ba5fa1ab6bbbd1d8acf5357e
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2020-02-23 10:57:20 -0800

    make the Get fix to selectors.py

--- src/python/pants/engine/selectors.py
+++ src/python/pants/engine/selectors.py
@@ -4,7 +4,8 @@
 import ast
 from dataclasses import dataclass
 from textwrap import dedent
-from typing import Any, Generator, Generic, Iterable, Optional, Tuple, Type, TypeVar, cast
+from typing import (Any, Generator, Generic, Iterable, List, Optional, Tuple, Type, TypeVar, Union,
+                    cast)
 
 from pants.util.meta import frozen_after_init
 from pants.util.objects import TypeConstraint
@@ -13,24 +14,24 @@ from pants.util.objects import TypeConstraint
 # engine will return from an `await Get[_Product](...)` expression. This type variable is also used
 # in the `Tuple[X, ...]` type returned by `await MultiGet(Get[X](...)...)`.
 _Product = TypeVar("_Product")
+_Params = TypeVar("_Params")
 
 
 @frozen_after_init
 @dataclass(unsafe_hash=True)
-class Get(Generic[_Product]):
+class Get(Generic[_Product, _Params]):
     """Experimental synchronous generator API.
 
-    May be called equivalently as either:   # verbose form: Get[product](subject_declared_type,
-    subject)   # shorthand form: Get[product](subject_declared_type(<constructor args for subject>))
+    May be called equivalently as either:
+    # verbose form: Get[product, subject_declared_type](subject)
+    # shorthand form: Get[product, subject_declared_type](subject_declared_type(<constructor args for subject>))
     """
 
-    product: Type[_Product]
-    # TODO: Consider attemping to create a Get[_Product, _Subject] which still allows for the 2-arg
-    # Get form, and then making this Type[_Subject]!
-    subject_declared_type: Type
-    subject: Optional[Any]
+    product_ype: Type[_Product]
+    param_types: Tuple[Type[Any], ...]
+    params: 'Params'
 
-    def __await__(self) -> "Generator[Get[_Product], None, _Product]":
+    def __await__(self) -> "Generator[Get[_Product, _Params], None, _Product]":
         """Allow a Get to be `await`ed within an `async` method, returning a strongly-typed result.
 
         The `yield`ed value `self` is interpreted by the engine within `extern_generator_send()` in
@@ -44,8 +45,8 @@ class Get(Generic[_Product]):
         - The engine will call `.send(None)` on the coroutine, which will either:
           - raise StopIteration with a value (if the coroutine `return`s), or
           - return a `Get` instance to the engine (if the rule instead called `await Get(...)`).
-        - The engine will fulfill the `Get` request to produce `x`, then call `.send(x)` and repeat the
-          above until StopIteration.
+        - The engine will fulfill the `Get` request to produce `x`, then call `.send(x)` and repeat
+          the above until StopIteration.
 
         See more information about implementing this method at
         https://www.python.org/dev/peps/pep-0492/#await-expression.
@@ -54,55 +55,58 @@ class Get(Generic[_Product]):
         return cast(_Product, result)
 
     @classmethod
-    def __class_getitem__(cls, product_type):
-        """Override the behavior of Get[T] to shuffle over the product T into the constructor
-        args."""
-        return lambda *args: cls(product_type, *args)
+    def _interpret_param_types(
+        cls,
+        product_type: Type[_Product],
+        param_types: Optional[Union[List[Type], Type]] = None,
+     ):
+        if param_types is None:
+            param_types = []
+        elif not isinstance(param_types, list):
+            param_types = [param_types]
 
-    def __init__(self, *args: Any) -> None:
-        # NB: Compat for Python 3.6, which doesn't recognize the __class_getitem__ override, but *does*
+        return dict(
+            product_type=product_type,
+            param_types=param_types,
+        )
+
+    @classmethod
+    def __class_getitem__(
+        cls,
+        product_type: Type[_Product],
+        param_types: Optional[Union[List[Type], Type]] = None,
+    ):
+      """Override the behavior of Get[T] to shuffle over the product T into the constructor args."""
+      return lambda *args: cls(*args, **cls._interpret_param_types(product_type, param_types))
+
+    def __init__(
+        self,
+        params: Optional[Union[List, Any]] = None,
+        product_type: Optional[Type[_Product]] = None,
+        param_types: Optional[Union[List[Type], Type]] = None,
+    ) -> None:        # NB: Compat for Python 3.6, which doesn't recognize the __class_getitem__ override, but *does*
         # contain an __orig_class__ attribute which is gone in later Pythons.
         # TODO: Remove after we drop support for running pants with Python 3.6!
         maybe_orig_class = getattr(self, "__orig_class__", None)
         if maybe_orig_class:
-            (type_param,) = maybe_orig_class.__args__
-            args = (type_param,) + args
+            assert product_type is None
+            assert param_types is None
+            product_type, param_types = maybe_orig_class.__args__
 
-        if len(args) not in (2, 3):
-            raise ValueError(
-                f"Expected either two or three arguments to {Get.__name__}; got {args}."
-            )
-        if len(args) == 2:
-            product, subject = args
+        assert product_type is not None
+        kwargs = self._interpret_param_types(product_type, param_types)
 
-            if isinstance(subject, (type, TypeConstraint)):
-                raise TypeError(
-                    dedent(
-                        """\
-                        The two-argument form of Get does not accept a type as its second argument.
+        self.product_type = cast(Type[_Product], kwargs['product_type'])
+        self.param_types = tuple(kwargs['param_types'] or ())
 
-                        args were: Get({args!r})
-
-                        Get.create_statically_for_rule_graph() should be used to generate a Get() for
-                        the `input_gets` field of a rule. If you are using a `await Get(...)` in a rule
-                        and a type was intended, use the 3-argument version:
-                        Get({product!r}, {subject_type!r}, {subject!r})
-                        """.format(
-                            args=args, product=product, subject_type=type(subject), subject=subject
-                        )
-                    )
-                )
-
-            subject_declared_type = type(subject)
-        else:
-            product, subject_declared_type, subject = args
-
-        self.product = product
-        self.subject_declared_type = subject_declared_type
-        self.subject = subject
+        if params is None:
+            params = ()
+        elif not isinstance(params, (list, tuple)):
+            params = (params,)
+        self.params = Params(*params)
 
     @staticmethod
-    def extract_constraints(call_node):
+    def extract_constraints(call_node: ast.Call) -> Tuple[str, Tuple[str, ...]]:
         """Parses a `Get(..)` call in one of its two legal forms to return its type constraints.
 
         :param call_node: An `ast.Call` node representing a call to `Get(..)`.
@@ -119,67 +123,53 @@ class Get(Generic[_Product]):
 
         # If the Get was provided with a type parameter, use that as the `product_type`.
         func = call_node.func
+        subscript_args: Tuple[Any, ...] = ()
         if isinstance(func, ast.Name):
             subscript_args = ()
         elif isinstance(func, ast.Subscript):
-            index_expr = func.slice.value
+            index_expr = func.slice.value # type: ignore[attr-defined]
             if isinstance(index_expr, ast.Name):
                 subscript_args = (index_expr,)
+            elif isinstance(index_expr, ast.Tuple):
+                subscript_args = tuple(index_expr.elts)
             else:
-                raise ValueError(f"Unrecognized type argument T for Get[T]: {ast.dump(index_expr)}")
+                raise ValueError(f'Unrecognized type arguments T, S... for Get[T, S...]: {ast.dump(index_expr)}')
         else:
             raise ValueError(
-                f"Unrecognized Get call node type: expected Get or Get[T], received {ast.dump(call_node)}"
-            )
+                f'Unrecognized Get call node type: expected Get[T] or Get[T, S...], received {ast.dump(call_node)}')
 
         # Shuffle over the type parameter to be the first argument, if provided.
-        combined_args = subscript_args + tuple(call_node.args)
-
-        if len(combined_args) == 2:
-            product_type, subject_constructor = combined_args
-            if not isinstance(product_type, ast.Name) or not isinstance(
-                subject_constructor, ast.Call
-            ):
-                raise ValueError(
-                    f"Two arg form of {Get.__name__} expected (product_type, subject_type(subject)), but "
-                    f"got: ({render_args(combined_args)})"
-                )
-            return (product_type.id, subject_constructor.func.id)
-        elif len(combined_args) == 3:
-            product_type, subject_declared_type, _ = combined_args
-            if not isinstance(product_type, ast.Name) or not isinstance(
-                subject_declared_type, ast.Name
-            ):
-                raise ValueError(
-                    f"Three arg form of {Get.__name__} expected (product_type, subject_declared_type, subject), but "
-                    f"got: ({render_args(combined_args)})"
-                )
-            return (product_type.id, subject_declared_type.id)
+        if len(subscript_args) == 1:
+            product_type, = subscript_args
+            return (product_type.id, ())
+        elif len(subscript_args) == 2:
+            product_type, param_types = subscript_args
+            if not isinstance(param_types, tuple):
+                param_types = (param_types,)
+            return (product_type.id, tuple(t.id for t in param_types))
         else:
-            raise ValueError(
-                f"Invalid {Get.__name__}; expected either two or three args, but "
-                f"got: ({render_args(combined_args)})"
-            )
+            raise ValueError(f'Invalid Get invocation: expected Get[T] or Get[T, S...], but '
+                             f'got: ({render_args(subscript_args)})')
 
     @classmethod
-    def create_statically_for_rule_graph(cls, product_type, subject_type) -> "Get":
+    def create_statically_for_rule_graph(cls, *args, **kwargs) -> "Get":
         """Construct a `Get` with a None value.
 
         This method is used to help make it explicit which `Get` instances are parsed from @rule
         bodies and which are instantiated during rule execution.
         """
-        return cls(product_type, subject_type, None)
+        return cls(*args, **kwargs)
 
 
 @frozen_after_init
 @dataclass(unsafe_hash=True)
-class MultiGet(Generic[_Product]):
+class MultiGet(Generic[_Product, _Params]):
     """Can be constructed with an iterable of `Get()`s and `await`ed to evaluate them in
     parallel."""
 
-    gets: Tuple[Get[_Product], ...]
+    gets: Tuple[Get[_Product, _Params], ...]
 
-    def __await__(self) -> Generator[Tuple[Get[_Product], ...], None, Tuple[_Product, ...]]:
+    def __await__(self) -> Generator[Tuple[Get[_Product, _Params], ...], None, Tuple[_Product, ...]]:
         """Yield a tuple of Get instances with the same subject/product type pairs all at once.
 
         The `yield`ed value `self.gets` is interpreted by the engine within `extern_generator_send()` in
@@ -193,7 +183,7 @@ class MultiGet(Generic[_Product]):
         result = yield self.gets
         return cast(Tuple[_Product, ...], result)
 
-    def __init__(self, gets: Iterable[Get[_Product]]) -> None:
+    def __init__(self, gets: Iterable[Get[_Product, _Params]]) -> None:
         """Create a MultiGet from a generator expression.
 
         This constructor will infer this class's _Product parameter from the input `gets`.
@@ -213,3 +203,6 @@ class Params:
 
     def __init__(self, *args: Any) -> None:
         self.params = tuple(args)
+
+    def __iter__(self):
+        return iter(self.params)
